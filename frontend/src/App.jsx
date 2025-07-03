92b8db4d46f5e4f3450b4a58a866dd5e06748511
Ahmed <akhanji01@gmail.com>
2025-07-03 18:12:34 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <h1> Book Website </h1>
+
+      <form>
+        <input type="text" placeholder='Book Title...'/>
+      </form>
+    </>
+  )
+}
