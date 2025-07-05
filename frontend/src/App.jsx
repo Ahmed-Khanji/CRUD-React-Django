50624fbe78c9d588c0fad0a51f671f4bec2c0c18
Ahmed <akhanji01@gmail.com>
2025-07-04 20:06:54 -0400

    try2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   const [books, setBooks] = useState([]);
   const [title, setTitle] = useState("");
   const [releaseYear, setReleaseYear] = useState(0);
+  let x;
 
   const fetchBooks = async () => {
     try {
